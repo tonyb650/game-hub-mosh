da49c3b5933c495e8f0cbd771e89deecc4943fb0
Tonyb650 <anthonylbrierly@gmail.com>
2024-05-30 09:52:07 -0700

    Fetch the games

---
+++ src/services/api-client.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+const RAWG_KEY = import.meta.env.VITE_RAWG_KEY;
+
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: RAWG_KEY
+  }
+})
