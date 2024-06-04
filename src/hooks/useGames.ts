c35a968300ce7a9742de6ba6273ae1a7623c4656
Tonyb650 <anthonylbrierly@gmail.com>
2024-06-04 10:14:57 -0700

    show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -25,19 +25,24 @@ const useGames = () => {
 
   const [games, setGames] = useState<Game[]>([])
   const [error, setError] = useState("")
+  const [isLoading, setLoading] = useState(false)
 
   useEffect(() => {
     const controller = new AbortController()
+    setLoading(true)
 
     apiClient
       .get<Games>("/games", {signal: controller.signal})
-      .then(res => setGames(res.data.results))
+      .then(res => {
+        setGames(res.data.results)
+        setLoading(false)})
       .catch(err => {
         if (err instanceof CanceledError ) {
           return
         } else {
           setError(err.message)
         }
+        setLoading(false)
       })
 
     return () => {
@@ -45,7 +50,7 @@ const useGames = () => {
     }
   },[])
 
-  return { games, error }
+  return { games, error, isLoading }
 }
 
 export default useGames
