8d1907930f49901bfc662fd36c674e3e4cc9e487
Tonyb650 <anthonylbrierly@gmail.com>
2024-06-10 17:37:38 -0700

    Show a spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,14 @@
-import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react'
+import { HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react'
 import useGenres from '../hooks/useGenres'
 import getCroppedImageUrl from '../services/image-url'
 
 
 const GenreList = () => {
   const {data, error, isLoading} = useGenres()
+
+  if (isLoading) return <Spinner/>
+  if (error) return null
+
   return (
     <List>
       {data && data.map((genre) => 
